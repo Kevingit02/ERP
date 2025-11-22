0d5fb906781fcbff3870f0e6f9d2877e69e42b8a
christinayoussef <christinaphoebeyoussef@gmail.com>
2025-11-22 16:35:02 +0100

    Update om.php

--- om.php
+++ om.php
@@ -1,1 +1,36 @@
-
+<?php
+    session_start();
+    if (empty($_SESSION['uid'])){
+      echo "Inte inloggad än, gå tillbaka till <a href='login.php'>inloggningssidan</a>";
+    }else{
+      $uid = $_SESSION['uid'];
+      $name = $_SESSION['name'];
+    }
+    if (!empty($uid)){
+?>
+<!DOCTYPE html>
+<html lang="sv">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Om oss – Mölndals vårdcentral</title>
+    <link href="style.css" rel="stylesheet">
+    <script src="javaScript.js"></script>
+  </head>
+  <body>
+       <header>
+      <div id="bannerContainer">
+        <div id="banner">
+          <a href="exempelfil_erp.php">Mölndals vårdcentral</a>
+        </div>
+        <div id="loggautDiv">
+          <a href="loggaut.php">Logga ut</a>
+        </div>
+      </div>
+      <nav>
+        <ul id="ulist">
+          <li><a href="exempelfil_erp.php" class="headerlist">Framsidan</a></li>
+          <li><a href="bokningar.php" class="headerlist">Bokade tider</a></li>
+          <li><a class="headerlist selected">Om oss</a></li>
+        </ul>
+      </nav>
+    </header>
